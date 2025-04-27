6be413a46dd931164db84b8babcc91c742235be8
Jorge Fox D'Angelo <jgdfox@gmail.com>
2025-04-26 22:19:53 -0300

    unregister service worker

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,19 @@ import "./index.css";
 import App from "./App.tsx";
 //import { registerSW } from "virtual:pwa-register";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then(function (registrations) {
+      for (const registration of registrations) {
+        registration.unregister();
+      }
+    })
+    .catch(function (err) {
+      console.error("Error al desregistrar Service Workers:", err);
+    });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
